60c6ae32b1f9386de845ce299324e82cbd6e8f83
Kirich8 <kirichenkomaxim@icloud.com>
2023-10-28 20:40:41 +0300

    feat/add loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,31 +2,43 @@ import './App.css';
 import React from 'react';
 import Header from '../components/header/Header';
 import Main from '../pages/main/Main';
-import { apiService } from '../utils/services/ApiServices';
 import CardNotFound from '../components/card-not-found/CardNotFound';
+import Loader from '../components/loader/Loader';
+import { apiService } from '../utils/services/ApiServices';
 
 class App extends React.Component {
   state = {
     characters: [],
+    isFetching: true,
   };
 
   changeCurrentCharacter = async (character: string) => {
     this.setState({
+      isFetching: true,
+    });
+
+    this.setState({
       characters: character
         ? await apiService.getCurrentCharacter(character)
         : await apiService.getCharacters(),
+      isFetching: false,
     });
   };
 
   async componentDidMount() {
-    this.setState({ characters: await apiService.getCharacters() });
+    this.setState({
+      characters: await apiService.getCharacters(),
+      isFetching: false,
+    });
   }
 
   render(): React.ReactNode {
     return (
       <>
         <Header changeCurrentCharacter={this.changeCurrentCharacter} />
-        {this.state.characters.length ? (
+        {this.state.isFetching ? (
+          <Loader />
+        ) : this.state.characters.length ? (
           <Main characters={this.state.characters} />
         ) : (
           <CardNotFound />
