51d709e6d60df3639e5988464de863c88b03ecc3
Kirich8 <kirichenkomaxim@icloud.com>
2023-11-30 20:14:19 +0300

    feat: add controlled form

--- src/pages/controlled-form/ControlledFormPage.tsx
+++ src/pages/controlled-form/ControlledFormPage.tsx
@@ -1,5 +1,94 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schema } from '../../utils/yup/schema';
+
+interface IFormProps {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+  conditions: boolean;
+}
+
 const ControlledFormPage = () => {
-  return <div>Controlled Form</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormProps>({
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+  });
+
+  const submitFormHandler: SubmitHandler<IFormProps> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="cfp">
+        <h2 className="cfp__title">Controled Form</h2>
+        <form className="cfp__form" onSubmit={handleSubmit(submitFormHandler)}>
+          <div className="input-box">
+            <input type="text" {...register('name')} autoComplete="off" placeholder="name" />
+            <label>Name: </label>
+            {errors.name && <p className="error-message">{errors.name.message}</p>}
+          </div>
+
+          <div className="input-box">
+            <input type="text" {...register('age')} autoComplete="off" placeholder="age" />
+            <label>Age: </label>
+            {errors.age && <p className="error-message">{errors.age.message}</p>}
+          </div>
+
+          <div className="input-box">
+            <input type="text" {...register('email')} autoComplete="off" placeholder="email" />
+            <label>Email: </label>
+            {errors.email && <p className="error-message">{errors.email.message}</p>}
+          </div>
+
+          <div className="input-box">
+            <input type="password" {...register('password')} autoComplete="off" placeholder="password" />
+            <label>Password: </label>
+            {errors.password && <p className="error-message">{errors.password.message}</p>}
+          </div>
+
+          <div className="input-box">
+            <input type="password" {...register('confirmPassword')} autoComplete="off" placeholder="confirmPassword" />
+            <label>Confirm password: </label>
+            {errors.confirmPassword && <p className="error-message">{errors.confirmPassword.message}</p>}
+          </div>
+
+          <div className="select-box">
+            <label>Gender: </label>
+            <select {...register('gender')}>
+              <option value="man">Man</option>
+              <option value="woman">Woman</option>
+            </select>
+            {errors.gender && <p className="error-message">{errors.gender.message}</p>}
+          </div>
+
+          <div className="checkbox">
+            <label>Accept T&C: </label>
+            <input type="checkbox" {...register('conditions')} />
+            {errors.conditions && <p className="error-message">{errors.conditions.message}</p>}
+          </div>
+
+          <div className="imagebox">
+            <label>Image: </label>
+            <input type="file" />
+            {/* {errors.conditions && <p className="error-message">{errors.conditions.message}</p>} */}
+          </div>
+
+          <button className="cfp__button" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default ControlledFormPage;
---
+++ src/utils/yup/schema.ts
@@ -0,0 +1,62 @@
+import * as yup from 'yup';
+
+export const schema = yup.object({
+  name: yup
+    .string()
+    .matches(/^[A-Z]/, {
+      message: 'Name must begin with a capital letter',
+      excludeEmptyString: true,
+    })
+    .required('Name is a required field'),
+
+  age: yup
+    .number()
+    .positive('Age must be a positive number')
+    .integer('Age must be an integer')
+    .transform((value, originalValue) => {
+      return originalValue === '' ? undefined : value;
+    })
+    .typeError('Age must be a number')
+    .required('Age is a required field'),
+
+  email: yup
+    .string()
+    .matches(/[a-zA-Z0-9._-]+@[a-zA-Z]{2,}\.[a-zA-Z]{2,}$/, {
+      message: 'Enter the email of the format: example@mi.us',
+      excludeEmptyString: true,
+    })
+    .email()
+    .required('Email is a required field'),
+
+  password: yup
+    .string()
+    .matches(/[A-Z]/, {
+      message: 'Password must include one uppercase letter',
+      excludeEmptyString: true,
+    })
+    .matches(/[a-z]/, {
+      message: 'Password must include one lowercase letter',
+      excludeEmptyString: true,
+    })
+    .matches(/[0-9]/, {
+      message: 'Password must include one digit',
+      excludeEmptyString: true,
+    })
+    .matches(/[^A-ZА-Яa-zа-я0-9Ёё\s]/, {
+      message: 'Password must include one special character',
+      excludeEmptyString: true,
+    })
+    .required('Password is a required field'),
+
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required('Passwords must match'),
+
+  gender: yup.string().required('Choose a gender').oneOf(['man', 'woman']),
+
+  conditions: yup
+    .boolean()
+    .oneOf([true], 'Confirm your agreement with the terms and conditions of the company')
+    .required(''),
+});
