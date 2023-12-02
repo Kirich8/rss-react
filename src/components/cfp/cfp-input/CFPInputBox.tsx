9931fff98426b4c72e7c90a417cb9179cd135952
Kirich8 <kirichenkomaxim@icloud.com>
2023-12-02 12:40:28 +0300

    feat: add password strenght

--- src/components/cfp/cfp-input/CFPInputBox.tsx
+++ src/components/cfp/cfp-input/CFPInputBox.tsx
@@ -1,6 +1,8 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
 import { InputFields } from './input-fields';
 import { countriesList } from '../../../utils/constants/countries-list';
+import { useState } from 'react';
+import { addPasswordStrenght } from '../../../utils/helpers/add-password-strenght';
 import DataList from './DataList';
 
 type CFPInputBoxProps = {
@@ -10,18 +12,25 @@ type CFPInputBoxProps = {
 };
 
 const CFPInputBox = ({ name, register, errorMessage }: CFPInputBoxProps) => {
+  const [value, setValue] = useState('');
+
   return (
     <>
       <div className="input-box">
         <input
+          value={value}
           type={name === InputFields.password || name === InputFields.confirmPassword ? 'password' : 'text'}
           {...register(name)}
           autoComplete="off"
           placeholder={name}
           list={name === InputFields.country ? 'list-of-countries' : ''}
+          onInput={(event) => setValue(event.currentTarget.value)}
         />
         <label>{name}:</label>
+
         <p className="error-message">{errorMessage}</p>
+
+        {name === InputFields.password && addPasswordStrenght(value)}
       </div>
 
       {name === InputFields.country && <DataList list={countriesList} />}
